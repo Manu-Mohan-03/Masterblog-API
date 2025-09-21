220adf58fec6b499d783012715c4491c345408de
Manu Mohan <manu.mohan0303@gmail.com>
2025-09-21 23:34:45 +0200

    Fixed the error when all posts are deleted and adding new pos

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -8,8 +8,8 @@ CORS(app)  # This will enable CORS for all routes
 
 # Global POST for sake of testing, can be moved to persistent storage as improvement
 POSTS = [
-    {"id": 10, "title": "First post", "content": "This is the first post."},
-    {"id": 20, "title": "Second post", "content": "This is the second post."},
+    {"id": 1, "title": "First post", "content": "This is the first post."},
+    {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
 
@@ -23,13 +23,13 @@ def validate_blog(blog_post):
 
 
 def clean_blog(blog_post):
-    """To remove unnecessary items from blog post"""
+    """To remove unnecessary items from blog post in case user sends
+        more element in json apart from title and content"""
     filtered_blog = {}
     if blog_post.get('title'):
         filtered_blog['title'] = blog_post.get('title')
     if blog_post.get('content'):
         filtered_blog['content'] = blog_post.get('content')
-
     return filtered_blog
 
 
@@ -58,7 +58,10 @@ def get_posts():
             return jsonify({"error": str(error)}), 400
         blog_post = clean_blog(blog_post)
         #Create new id
-        new_id = max([post['id'] for post in POSTS]) + 10
+        if POSTS:
+            new_id = max([post['id'] for post in POSTS]) + 1
+        else: # Added as bug fix to handle the case; if all posts got deleted
+            new_id = 1
         blog_post['id'] = new_id
 
         # Add post to blogs list
@@ -68,11 +71,12 @@ def get_posts():
         # retrieve the query parameters
         sort = request.args.get('sort', None)
         direction = request.args.get('direction', None)
-        if sort in ('title', 'content'):
+        if sort in ('title', 'content') and direction in ('asc', 'desc'):
             # To keep the original intact
             results = POSTS.copy()
-            if not direction or direction == 'asc':
+            #if not direction or direction == 'asc':
                 # if direction not mentioned by default ascending
+            if direction == 'asc':
                 results.sort(key=lambda post: post.get(sort, ''))
             elif direction == 'desc':
                 # to sort in descending order
@@ -99,7 +103,10 @@ def update_post(post_id):
         return jsonify({"error": "Not found"}), 404
     # get the data from body of the request
     new_post = request.get_json()
+    # Keep only title and content in the new_post
     new_post = clean_blog(new_post)
+    # To check if new_post is a subset of existing post (exising post contain post_id,
+    # may of may not contain post_id)
     if new_post.items() <= blog_post.items():
         return jsonify({"info": "Nothing to update"})
     blog_post.update(new_post)
@@ -112,15 +119,15 @@ def search_post():
     """
     For searching a post using title or content
     Search will return or contents not and contents
-    For eg: title=ab&content=cd will show all posts with title ab and
-    all the posts with content cd. it will not restrict to only show
+    For eg: title=ab&content=cd will show all posts with title 'ab' and
+    all the posts with content 'cd'. it will not restrict to only show
     the post with title ab and content cd.
     """
     title = request.args.get('title', None)
     content = request.args.get('content', None)
-
     result = []
     for post in POSTS:
+        print(post)
         if title and title.lower() in post['title'].lower():
             result.append(post)
         if content and content.lower() in post['content'].lower():
